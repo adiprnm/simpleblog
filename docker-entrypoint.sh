f59c4de1141b7bd970b6f84ffd8ec91155ca0588
Adi Purnama <adiprnm2014@gmail.com>
2025-03-06 22:30:59 +0700

    Update .gitignore and docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Ensure the app has the correct permissions
 chown -R nobody:nogroup /app
 
-if [ "${@:1:1}" == "bundle" ] && [ "${@: 2:1}" == "exec" && "${@: 3:1}" == "puma" ]; then
+if [ "${1}" == "bundle" ] && [ "${2}" == "exec" && "${3}" == "puma" ]; then
   if [ ! -f storage/db.sqlite3 ]; then
     echo "Creating database..."
     touch storage/db.sqlite3
