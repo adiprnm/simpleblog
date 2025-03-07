1957beb895950372ea40dc7fc32e6320317bfe97
Adi Purnama <adiprnm2014@gmail.com>
2025-03-07 13:31:31 +0700

    Refine predefined data

--- predefined_data.rb
+++ predefined_data.rb
@@ -4,19 +4,21 @@ require_relative './database'
 
 db = create_database_connection
 db.execute <<-SQL
-  INSERT INTO pages (id, title, slug, content, 'state', 'published_at') VALUES (1, "Home", 'home', 'Welcome!\n\nHappy blogging!', 'published', 'NOW()') ON CONFLICT DO NOTHING;
+  INSERT INTO pages (id, title, slug, content, 'state', 'published_at')
+  VALUES (1, 'Home', 'home', 'Welcome!\n\nHappy blogging!', 'published', datetime('now'))
+  ON CONFLICT DO UPDATE SET published_at = datetime('now') WHERE id = excluded.id;
 SQL
 
 db.execute <<-SQL
-  INSERT INTO pages (id, title, slug, content, 'state', 'published_at') VALUES (2, "Example Page", 'example-page', 'This is an example page', 'published', 'NOW()') ON CONFLICT DO NOTHING;
+  INSERT INTO pages (id, title, slug, content, 'state', 'published_at')
+  VALUES (2, "Example Page", 'example-page', 'This is an example page', 'published', datetime('now'))
+  ON CONFLICT DO UPDATE SET published_at = datetime('now') WHERE id = excluded.id;
 SQL
 
 db.execute <<-SQL
-  INSERT INTO posts
-    (id, title, slug, content, 'state', 'published_at')
-  VALUES
-    (1, "Hello World!", 'hello-world', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et venenatis mi. Duis accumsan quam ac nulla sagittis interdum. Mauris aliquet facilisis sapien, eget luctus turpis vehicula ac. Integer porttitor ex at risus pretium rutrum. Suspendisse euismod mi in dolor fringilla, nec lacinia enim euismod. Donec quis sagittis eros, sit amet dignissim lectus. Pellentesque id lacinia sem. Vestibulum tellus ipsum, venenatis nec tortor at, pretium egestas ex. Maecenas vitae pretium mi. Etiam nec augue et quam semper consectetur vel vel tortor.', 'published', "'#{Time.now}'")
-  ON CONFLICT DO NOTHING;
+  INSERT INTO posts (id, title, slug, content, 'state', 'published_at')
+  VALUES (1, "Hello World!", 'hello-world', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et venenatis mi. Duis accumsan quam ac nulla sagittis interdum. Mauris aliquet facilisis sapien, eget luctus turpis vehicula ac. Integer porttitor ex at risus pretium rutrum. Suspendisse euismod mi in dolor fringilla, nec lacinia enim euismod. Donec quis sagittis eros, sit amet dignissim lectus. Pellentesque id lacinia sem. Vestibulum tellus ipsum, venenatis nec tortor at, pretium egestas ex. Maecenas vitae pretium mi. Etiam nec augue et quam semper consectetur vel vel tortor.', 'published', datetime('now'))
+  ON CONFLICT DO UPDATE SET published_at = datetime('now') WHERE id = excluded.id;
 SQL
 
 db.execute <<-SQL
@@ -25,12 +27,12 @@ SQL
 db.execute <<-SQL
   INSERT INTO navlinks (id, name, position, url) VALUES (2, 'Blog', 2, '/blog') ON CONFLICT DO NOTHING;
 SQL
+db.execute <<-SQL
+  INSERT INTO navlinks (id, name, position, url) VALUES (3, 'Example Page', 2, '/example-page') ON CONFLICT DO NOTHING;
+SQL
 
 db.execute <<-SQL
   INSERT INTO settings (id, key, value) VALUES (1, 'site.title', "Lorem Ipsum") ON CONFLICT DO NOTHING;
-SQL
-db.execute <<-SQL
-  INSERT INTO settings (id, key, value) VALUES (2, 'site.base_url', "https://example.com") ON CONFLICT DO NOTHING;
 SQL
 
 db.close
