32a1bcce7249f15fdac80631c8ac549168c529eb
Adi Purnama <adiprnm2014@gmail.com>
2025-03-09 22:12:22 +0700

    Footer text settings

--- predefined_data.rb
+++ predefined_data.rb
@@ -10,25 +10,10 @@ db.execute <<-SQL
 SQL
 
 db.execute <<-SQL
-  INSERT INTO pages (id, title, slug, content, 'state', 'published_at')
-  VALUES (2, "Example Page", 'example-page', 'This is an example page', 'published', datetime('now'))
-  ON CONFLICT DO NOTHING;
-SQL
-
-db.execute <<-SQL
-  INSERT INTO posts (id, title, slug, content, 'state', 'published_at')
-  VALUES (1, "Hello World!", 'hello-world', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et venenatis mi. Duis accumsan quam ac nulla sagittis interdum. Mauris aliquet facilisis sapien, eget luctus turpis vehicula ac. Integer porttitor ex at risus pretium rutrum. Suspendisse euismod mi in dolor fringilla, nec lacinia enim euismod. Donec quis sagittis eros, sit amet dignissim lectus. Pellentesque id lacinia sem. Vestibulum tellus ipsum, venenatis nec tortor at, pretium egestas ex. Maecenas vitae pretium mi. Etiam nec augue et quam semper consectetur vel vel tortor.', 'published', datetime('now'))
-  ON CONFLICT DO NOTHING;
-SQL
-
-db.execute <<-SQL
   INSERT INTO navlinks (id, name, position, url) VALUES (1, 'Home', 1, '/') ON CONFLICT DO NOTHING;
 SQL
 db.execute <<-SQL
   INSERT INTO navlinks (id, name, position, url) VALUES (2, 'Blog', 2, '/blog') ON CONFLICT DO NOTHING;
-SQL
-db.execute <<-SQL
-  INSERT INTO navlinks (id, name, position, url) VALUES (3, 'Example Page', 2, '/example-page') ON CONFLICT DO NOTHING;
 SQL
 
 db.execute <<-SQL
@@ -40,6 +25,10 @@ db.execute <<-SQL
 SQL
 db.execute <<-SQL
   INSERT INTO settings (id, key, value) VALUES (3, 'site.twitter.username', "john_doe")
+  ON CONFLICT DO NOTHING;
+SQL
+db.execute <<-SQL
+  INSERT INTO settings (id, key, value) VALUES (4, 'site.text.footer', 'Written by human. Grab the [RSS Feed](/feed.xml)')
   ON CONFLICT DO NOTHING;
 SQL
 
