0cb93d8e23fb292a423811305742e8aead7ff8af
Adi Purnama <adiprnm2014@gmail.com>
2025-03-16 13:52:10 +0700

    Limit and order params should be optional

--- renderer.rb
+++ renderer.rb
@@ -14,26 +14,27 @@ class Renderer
   end
 
   def render_recent_posts
-    @rendered_text = @rendered_text.gsub(/\{\{\s+recent_posts\s+limit:\s+(\d+),\s+order:\s+"([^"]+)"\s+\}\}/) do |match|
+    @rendered_text = @rendered_text.gsub(/\{\{\s+recent_posts(?:\s+limit:\s+(\d+))?\s+\}\}/) do |match|
       limit = Regexp.last_match(1) || 6
-      order = Regexp.last_match(2) || 'published_at'
-      posts = @db.execute("SELECT title, slug FROM posts WHERE state = 'published' ORDER BY ? LIMIT ?", [order, limit])
+      posts = @db.execute("SELECT title, slug FROM posts WHERE state = 'published' ORDER BY published_at DESC LIMIT ?", limit)
       posts.map { |post| "- [#{post['title']}](/#{post['slug']})" }.join("\n")
     end
     self
   end
 
   def render_popular_posts
-    @rendered_text = @rendered_text.gsub(/\{\{\s+popular_posts\s+limit:\s+(\d+),\s+order:\s+"([^"]+)"\s+\}\}/) do |match|
+    @rendered_text = @rendered_text.gsub(/\{\{\s+popular_posts(?:\s+limit:\s+(\d+))?(?:,\s+order:\s+"([^"]+)")?\s+\}\}/) do |match|
       limit = Regexp.last_match(1) || 6
       order = Regexp.last_match(2) || 'count'
-      posts = @db.execute(<<-SQL, limit: limit)
-        SELECT posts.title, posts.slug, COUNT(*) AS count
-        FROM visits
-        INNER JOIN posts ON visits.entry_id = posts.id AND visits.entry_type = 'post'
-        GROUP BY posts.id
-        ORDER BY count DESC
-        LIMIT :limit
+      posts = @db.execute(<<-SQL, limit: limit, order: order)
+        SELECT * FROM (
+          SELECT posts.title, posts.slug, COUNT(*) AS count
+          FROM visits
+          INNER JOIN posts ON visits.entry_id = posts.id AND visits.entry_type = 'post'
+          GROUP BY posts.id
+          ORDER BY count DESC
+          LIMIT :limit
+        ) popular_posts ORDER BY :order
       SQL
       posts.map { |post| "- [#{post['title']}](/#{post['slug']})" }.join("\n")
     end
